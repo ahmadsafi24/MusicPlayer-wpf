9fe7596b8030a3f70a2f31a277164ca0f8530408
ahmadsafi24 <ahmad3afy@Gmail.com>
2021-10-26 00:57:48 -0700

    added;switch to blur/to normal window

--- PlayerUI/Commands/ViewSwitcher.cs
+++ PlayerUI/Commands/ViewSwitcher.cs
@@ -21,5 +21,31 @@ namespace PlayerUI.Commands
             Application.Current.MainWindow = MainWindow;
             lastwin.Close();
         }
+
+        public static void SwitchToBlurWindow()
+        {
+            var lastwin = Application.Current.MainWindow;
+            Windows.MainWindowChromeless Chromeless = new();
+            Chromeless.Width=lastwin.Width;
+            Chromeless.Height=lastwin.Height;
+            Chromeless.Left=lastwin.Left;
+            Chromeless.Top=lastwin.Top;
+            Chromeless.Show();
+            Application.Current.MainWindow = Chromeless;
+            lastwin.Close();
+        }
+        
+        public static void SwitchToNormalWindow()
+        {
+            var lastwin = Application.Current.MainWindow;
+            Windows.MainWindow win = new();
+            win.Width=lastwin.Width;
+            win.Height=lastwin.Height;
+            win.Left=lastwin.Left;
+            win.Top=lastwin.Top;
+            win.Show();
+            Application.Current.MainWindow = win;
+            lastwin.Close();
+        }
     }
 }
--- PlayerUI/ViewModel/MenuViewModel.cs
+++ PlayerUI/ViewModel/MenuViewModel.cs
@@ -8,12 +8,15 @@ namespace PlayerUI.ViewModel
         public ICommand OpenCommand { get; }
         public ICommand ToggleDarkModeCommand { get; }
         public ICommand SwitchToMiniViewCommand { get; }
-
+        public ICommand SwitchToNormalWindowCommand { get; }
+        public ICommand SwitchToBlurWindowCommand { get; }
         public MenuViewModel()
         {
             OpenCommand = new DelegateCommand(() => Commands.FilePicker.OpenFilePicker());
             ToggleDarkModeCommand = new DelegateCommand(() => Commands.WindowTheme.DarkThemeToggle());
             SwitchToMiniViewCommand = new DelegateCommand(() => Commands.ViewSwitcher.SwitchToMiniView());
+            SwitchToBlurWindowCommand=new DelegateCommand(()=>Commands.ViewSwitcher.SwitchToBlurWindow());
+        SwitchToNormalWindowCommand=new DelegateCommand(()=>Commands.ViewSwitcher.SwitchToNormalWindow());
         }
     }
 }
